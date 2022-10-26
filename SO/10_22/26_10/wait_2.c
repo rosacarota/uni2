0c67dbb3f8b6cf01934c66274d09fc07abecc057
Carota <rosa.carota.01@gmail.com>
2022-10-26 15:28:55 +0200

    modified:   wait_2.c

--- SO/10_22/26_10/wait_2.c
+++ SO/10_22/26_10/wait_2.c
@@ -7,7 +7,7 @@ int main(void) {
 
    value1 = fork(); 
 
-    if (value1 < 0) return 0;
+    if (value1 < 0) exit(0);
 
     if (value1 == 0) {
         printf("PID primo figlio: %d\n", getpid());
@@ -16,23 +16,27 @@ int main(void) {
             printf("%d ", i);
         }
         putchar('\n');
+
+        exit(0);
     }
     else if (value1 > 0) {
         value2 = fork();
         
-        if (value2 < 0) return 0;
+        if (value2 < 0) exit(0);
 
         if (value2 == 0) {
-            printf("PID secondo figlio: %d\n", getpid());
+            printf("PID padre dal secondo figlio: %d\n", getppid());
 
             for(int i = 101; i <= 150; i++) {
                 printf("%d ", i);
             }
             putchar('\n');
+
+            exit(0);
         }
         else if (value2 > 0) {
-            printf("PID primo figlio terminato dal padre: %d\n", waitpid(-1, NULL, 0));
-            printf("PID secondo figlio terminato dal padre: %d\n", waitpid(-1, NULL, 0));
+            printf("PID primo figlio terminato dal padre: %d\n", waitpid(value1, NULL, 0));
+            printf("PID secondo figlio terminato dal padre: %d\n", waitpid(value2, NULL, 0));
         }
     }
 
